d8ba6c982665a914e3a844bdbc5eefaf03919052
Notizen__ <66460003+notizen-00@users.noreply.github.com>
2025-08-04 06:32:19 +0700

    Update index.vue
    fd

--- src/pages/prediction/index.vue
+++ src/pages/prediction/index.vue
@@ -121,7 +121,7 @@ async function predict() {
   isLoading.value = true;
 
   try {
-    const response = await fetch('https://baa2d25241c3.ngrok-free.app/predict', {
+    const response = await fetch('https://b7292ba9b54e.ngrok-free.app/predict', {
       method: 'POST',
       body: formData,
     });
